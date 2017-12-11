e874374d1eae85ef89ec314277adf0106dca6ac9
Pascal Rhod <pascal.rhod@canaltp.fr>
2017-12-11 18:14:48 +0100

    add line indicators

--- indicators_generation/indicators_generation.py
+++ indicators_generation/indicators_generation.py
@@ -8,6 +8,8 @@ import requests
 import numpy as np
 from io import BytesIO
 from shapely.geometry import LineString
+import csv
+import datetime
 
 def get_lines_speed(gtfs_source):
     with zipfile.ZipFile(gtfs_source, 'r') as myzip:
@@ -101,18 +103,72 @@ def get_trip_frequencies(gtfs_source):
 
     return pd.DataFrame(trips_count)
 
-def get_lines_total_distance_and_frequency(gtfs_source):
+def get_calendar_active_days(gtfs_source):
+    # Loading all activated days for a service_id in a dictionnary
+    service_days = {}
+    with zipfile.ZipFile(gtfs_source, 'r') as myzip:
+        calendar_data = pd.read_csv(myzip.open('calendar.txt'))
+        calendar_data["start_date"] = calendar_data["start_date"].apply(str)
+        calendar_data["start_date"] = pd.to_datetime(calendar_data["start_date"], format='%Y%m%d')
+        calendar_data["end_date"] = calendar_data["end_date"].apply(str)
+        calendar_data["end_date"] = pd.to_datetime(calendar_data["end_date"], format='%Y%m%d')
+        for row in calendar_data.iterrows():
+            service_id = row[1]['service_id']
+            if not service_id in service_days:
+                service_days[service_id] = []
+            start_date = row[1]["start_date"]
+            end_date = row[1]["end_date"]
+            current = start_date
+            while current <= end_date:
+                current_is_active = (
+                    (current.weekday() == 0 and row[1]['monday'] == 1) or
+                    (current.weekday() == 1 and row[1]['tuesday'] == 1) or
+                    (current.weekday() == 2 and row[1]['wednesday'] == 1) or
+                    (current.weekday() == 3 and row[1]['thursday'] == 1) or
+                    (current.weekday() == 4 and row[1]['friday'] == 1) or
+                    (current.weekday() == 5 and row[1]['saturday'] == 1) or
+                    (current.weekday() == 6 and row[1]['sunday'] == 1))
+                if current_is_active and current not in service_days[service_id]:
+                    service_days[service_id].append(current)
+                current = current + datetime.timedelta(days=1)
+        if 'calendar_dates.txt' in myzip.namelist():
+            calendar_dates_data = pd.read_csv(myzip.open('calendar_dates.txt'))
+            calendar_dates_data["date"] = calendar_dates_data["date"].apply(str)
+            calendar_dates_data["date"] = pd.to_datetime(calendar_dates_data["date"], format='%Y%m%d')
+            for row in calendar_dates_data.iterrows():
+                service_id = row[1]["service_id"]
+                current_date = row[1]["date"]
+                if row[1]["exception_type"] == 1 and current_date not in service_days[service_id]:
+                    service_days[service_id].append(current_date)
+                elif row[1]["exception_type"] == 2 and current_date in service_days[service_id]:
+                    service_days[service_id].remove(current_date)
+    # formating the resuting data to ease the DataFrame creation
+    serice_data = []
+    for service_id, dates in service_days.items():
+        min_date = min(dates)
+        max_date = max(dates)
+        serice_data.append({
+            "service_id": service_id,
+            "start_date": min_date,
+            "end_date": max_date,
+            "active_days_count": len(dates)
+        })
+    return pd.DataFrame(serice_data)
+
+
+def get_trips_detailed_infos(gtfs_source):
     with zipfile.ZipFile(gtfs_source, 'r') as myzip:
         trips_file = myzip.open('trips.txt')
         trips_data = pd.read_csv(trips_file)
 
     shape_infos = get_shape_infos(gtfs_source)
     frequencies_infos = get_trip_frequencies(gtfs_source)
-    frequencies_infos.to_sql("frequencies_data", db_conn, if_exists="replace")
+    service_data = get_calendar_active_days(gtfs_source)
 
     trips_data = trips_data.merge(shape_infos, how='left', on="shape_id")
     trips_data = trips_data.merge(frequencies_infos, how='left', on="trip_id")
     trips_data["trip_daily_count"] = trips_data["trip_daily_count"].fillna(1)
+    trips_data = trips_data.merge(service_data, how='left', on="service_id")
     return trips_data
 
 
@@ -123,6 +179,42 @@ def db_table_exists(db_connection, table_name):
     rows = cur.fetchall()
     return len(rows) > 0
 
+def get_line_validity(trips_source_data):
+    # for each route_id, compute the first and last active day
+    grouped_trips = trips_source_data.groupby('route_id')
+    route_infos = []
+    for route_id in grouped_trips.groups:
+        trips = grouped_trips.get_group(route_id)
+        min_date = min(trips["start_date"]).to_datetime()
+        max_date = min(trips["end_date"]).to_datetime()
+        delta = (max_date - min_date).days + 1
+        route_infos.append( {
+            "route_id": route_id,
+            "min_date" : min_date,
+            "max_date" : max_date,
+            "total_day_count": delta
+        })
+    return pd.DataFrame(route_infos)
+
+def get_line_complementary_infos_from_trips(trips_source_data):
+    # for each route_id, compute yearly traveled distance
+    line_validity = get_line_validity(trips_source_data)
+    trips_data = trips_source_data.merge(line_validity, how='left', on="route_id")
+    trips_data["trip_yearly_count"] = (trips_data["trip_daily_count"] *
+        trips_data["active_days_count"] / 365 *
+        trips_data["total_day_count"]
+    )
+    trips_data["trip_yearly_length"] = trips_data["shape_length_in_meter"] * trips_data["trip_yearly_count"]
+    grouped_trips = trips_data.groupby('route_id')
+    route_infos = []
+    for route_id in grouped_trips.groups:
+        trips = grouped_trips.get_group(route_id)
+        total_length = sum(trips["trip_yearly_length"])
+        route_infos.append( {
+            "route_id": route_id,
+            "yearly_distance_km": total_length / 1000
+        })
+    return pd.DataFrame(route_infos)
 
 if __name__ == "__main__":
     gtfs_source = "https://github.com/AFDLab4Dev/AccraMobility/raw/master/GTFS/GTFS_Accra.zip"
@@ -134,10 +226,14 @@ if __name__ == "__main__":
 
     db_conn = sqlite3.connect(dest_db)
 
-    trips_data = get_lines_total_distance_and_frequency('/home/prhod/github/accra_dashboard/data/GTFS_Accra.zip')
-    trips_data.to_sql("trips_data", db_conn, if_exists="replace")
-    exit()
+    # gtfs_file = '/home/prhod/github/accra_dashboard/data/GTFS_Accra.zip'
     r = requests.get(gtfs_source)
-    lines_data = get_lines_infos(BytesIO(r.content), CO2_source, 150)
+    gtfs_file = BytesIO(r.content)
+    trips_data = get_trips_detailed_infos(gtfs_file)
+    trips_data.to_sql("trips_data", db_conn, if_exists="replace")
+    line_complementary_infos_from_trips = get_line_complementary_infos_from_trips(trips_data)
+    lines_data = get_lines_infos(gtfs_file, CO2_source, 150)
+    lines_data = lines_data.merge(line_complementary_infos_from_trips, how='left', on="route_id")
+    lines_data["total_co2"] = lines_data["yearly_distance_km"] * lines_data["co2_per_km"]
     # stoppoint avec lien vers ligne
     lines_data.to_sql("lines_infos", db_conn, if_exists="replace")
