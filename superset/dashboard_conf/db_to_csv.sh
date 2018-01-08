0c01675ac5777967450994a2f20aea92418be1f1
vpapailio <virginia.papailiopoulou@gmail.com>
2018-01-08 19:12:59 +0100

    Update layout + exported csv tables

--- superset/dashboard_conf/db_to_csv.sh
+++ superset/dashboard_conf/db_to_csv.sh
@@ -1,3 +1,7 @@
 sqlite3 -header -csv superset/superset.db "select * from dashboards;" > superset/dashboard_conf/dashboards.csv
 sqlite3 -header -csv superset/superset.db "select * from dashboard_user;" > superset/dashboard_conf/dashboard_user.csv
 sqlite3 -header -csv superset/superset.db "select * from dashboard_slices;" > superset/dashboard_conf/dashboard_slices.csv
+sqlite3 -header -csv superset/superset.db "select * from tables;" > superset/dashboard_conf/tables.csv
+sqlite3 -header -csv superset/superset.db "select * from slices;" > superset/dashboard_conf/slices.csv
+sqlite3 -header -csv superset/superset.db "select * from saved_query;" > superset/dashboard_conf/saved_query.csv
+
